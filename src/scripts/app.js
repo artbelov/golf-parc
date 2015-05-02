3114c5a4018b7296a2b9b7889aab57e5ccb20ff9
Artem Belov <artbelov@live.ru>
2015-05-02 15:15:55 +0400

    Correct injection of 3rd-party libs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,14 +1,14 @@
-/* global angular */
-
 (function () {
   'use strict';
 
   /* @ngInject */
-  function MainCtrl ($scope) {
-    console.log($scope)
+  function MainCtrl ($, _) {
+    console.log('Angular: ' + angular.version.full + ', jQuery: ' + $.fn.jquery + ', Lo-Dash:' + _.VERSION + ', Modernizr: ' + Modernizr._version);
   }
 
   angular
     .module('golf-parc', [])
+    .constant('$', $.noConflict())
+    .constant('_', _.noConflict())
     .controller('MainCtrl', MainCtrl);
 })();
